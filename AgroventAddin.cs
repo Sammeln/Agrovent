120b42b95e46fa09bb138f796f6ce48711d4f477
Sammeln <40733234+Sammeln@users.noreply.github.com>
2025-11-06 13:09:26 +0300

    Реализация ServiceContainer  для дальнейшего исползования DI

--- AgroventAddin.cs
+++ AgroventAddin.cs
@@ -1,19 +1,45 @@
 
+using System.Runtime.InteropServices;
 using Xarial.XCad.SolidWorks;
+using Xarial.XCad.UI.Commands;
 
 namespace Agrovent
 {
+    [ComVisible(true)]
     public class AgroventAddin : SwAddInEx
     {
         public override void OnConnect()
         {
+            CommandManager.AddCommandGroup<Commands_e>().CommandClick += OnCommandClickExecute;
             // Initialization code here
         }
 
+        private void OnCommandClickExecute(Commands_e command)
+        {
+            switch (command)
+            {
+                case Commands_e.Command1:
+                    // Handle Command1
+                    break;
+                case Commands_e.Command2:
+                    // Handle Command2
+                    break;
+                
+                default:
+                    break;
+            }
+        }
         public override void OnDisconnect()
         {
             // Cleanup code here
         }
+    }
+
+
+    public enum Commands_e
+    {
+        Command1,
+        Command2
     }
 
 }
