2463073f0b376ab16cf95f72e8642aff4419a76e
Sammeln <40733234+Sammeln@users.noreply.github.com>
2025-11-06 10:15:25 +0300

    Начало проекта

---
+++ AgroventAddin.cs
@@ -0,0 +1,19 @@
+
+using Xarial.XCad.SolidWorks;
+
+namespace Agrovent
+{
+    public class AgroventAddin : SwAddInEx
+    {
+        public override void OnConnect()
+        {
+            // Initialization code here
+        }
+
+        public override void OnDisconnect()
+        {
+            // Cleanup code here
+        }
+    }
+
+}
