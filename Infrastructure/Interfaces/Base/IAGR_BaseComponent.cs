344e68703f8d2aa5b9e10c769bafe58724ca1ccd
Sammeln <40733234+Sammeln@users.noreply.github.com>
2025-11-26 12:40:45 +0300

    Добавил DataContext

---
+++ Infrastructure/Interfaces/Base/IAGR_BaseComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agrovent.Infrastructure.Interfaces.Base
+{
+    public interface IAGR_BaseComponent
+    {
+        abstract string Name { get; set; }
+        abstract AvaArticleModel AvaArticle { get; set; }
+        abstract string PartNumber { get; set; }
+        abstract ComponentType_e Type { get; set; }
+        abstract int Version { get; set; }
+        abstract IPropertiesCollection PropertiesCollection { get; set; }
+
+    }
